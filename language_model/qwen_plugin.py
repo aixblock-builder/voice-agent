56638958a1c57da8467cfcb7367323905ba245ab
mourinhan <sbtc.walkingdead@gmail.com>
2025-08-14 15:28:16 +0700

    qwen plugin

---
+++ language_model/qwen_plugin.py
@@ -0,0 +1,35 @@
+from language_model.llm_base import LlmBase, register_llm_plugin
+
+
+@register_llm_plugin("qwen")
+class QwenPlugin(LlmBase):
+    def __init__(self, model_id: str, **kwargs):
+        super().__init__(model_id, **kwargs)
+        self.model_id = model_id
+        self.language = kwargs.get("language", "en")
+    
+    def load_pipeline(self, **kwargs):
+        from transformers import AutoModelForCausalLM, AutoTokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(self._get_model_source(), **kwargs)
+        self.pipeline = AutoModelForCausalLM.from_pretrained(self._get_model_source(), device_map="auto", **kwargs)
+        return self
+    
+    def load_tokenizer(self, **kwargs):
+        from transformers import AutoTokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(self._get_model_source(), **kwargs)
+        return self
+    
+    def generate(self, prompt: str, **kwargs) -> str:
+        inputs = self.tokenizer(prompt, return_tensors="pt").to(self.device)
+        outputs = self.pipeline.generate(
+            inputs["input_ids"],
+            max_length=kwargs.get("max_length", 512),
+            num_beams=kwargs.get("num_beams", 1),
+            do_sample=kwargs.get("do_sample", False),
+            temperature=kwargs.get("temperature", 1.0)
+        )
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
+    def save_model(self, path: str):
+        self.pipeline.save_pretrained(path)
+        self.tokenizer.save_pretrained(path)
